5ed454bbe038da28aa05be98efe1d92f8c13399d
npark080 <npark080@gmail.com>
2024-10-24 00:19:53 -0500

    Implemented entity framework

---
+++ LifeFlow.DATA.EF/Models/CourseStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LifeFlow.DATA.EF.Models
+{
+    public partial class CourseStatus
+    {
+        public CourseStatus()
+        {
+            Courses = new HashSet<Course>();
+        }
+
+        public int CourseStatusId { get; set; }
+        public string CourseStatusName { get; set; } = null!;
+
+        public virtual ICollection<Course> Courses { get; set; }
+    }
+}
---
+++ LifeFlow.DATA.EF/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LifeFlow.DATA.EF.Models
+{
+    public partial class Student
+    {
+        public Student()
+        {
+            Enrollments = new HashSet<Enrollment>();
+        }
+
+        public int StudentId { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? Phone { get; set; }
+        public string Email { get; set; } = null!;
+        public string? Notes { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int StudentStatusId { get; set; }
+
+        public virtual StudentStatus StudentStatus { get; set; } = null!;
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
